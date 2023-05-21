cd853f1462a81a845c1c14080f392053296a4ed0
Denis Kurov <kurovdenis1987@gmail.com>
2023-05-21 22:34:27 +0400

    Добавил проект на получение праздничных дней

--- Program.cs
+++ Program.cs
@@ -1,52 +1,61 @@
-﻿namespace MyStudyProject
+﻿using System;
+
+namespace MyStudyProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] numberList = { 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+            int[] numberList = new int[101];
+            for (int i = 0; i < numberList.Length; i++)
+            {
+                numberList[i] = i;
+            }
 
             string numberString;
             int low = 0;
             int high = numberList.Length - 1;
             int thisNumber = 0;
             int guess = 0;
-
+            int iterationCount = 0;
 
             Console.WriteLine($"Guess : {thisNumber}, Find : {numberList[guess]}, Length: {high}");
 
-            while (thisNumber < 15 || thisNumber > 30)
-            try
+            while (thisNumber < 1 || thisNumber > 100)
             {
-                    Console.WriteLine("Введите число от 15 до 30 :");
+                try
+                {
+                    Console.WriteLine("Введите число от 1 до 100:");
                     numberString = Console.ReadLine();
                     thisNumber = Convert.ToInt32(numberString);
-
+                }
+                catch
+                {
+                    continue;
+                }
             }
-            catch
-            {
-                continue;
-            }
-
 
             while (numberList[guess] != thisNumber)
             {
-                guess = (low + high)/2;
+                guess = (low + high) / 2;
+                iterationCount++;
+
                 if (thisNumber < numberList[guess])
                 {
                     high = guess - 1;
                 }
-
                 else if (thisNumber > numberList[guess])
                 {
                     low = guess + 1;
                 }
 
 
+                Console.WriteLine($"Guess : {thisNumber}, Find : {numberList[guess]}, Count : {iterationCount}, Range: {numberList[low]} - {numberList[high]} ");
             }
 
-            Console.WriteLine($"Guess : {thisNumber}, Find : {numberList[guess]}, Length: {high}");
-
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine($"Guess : {thisNumber}, Find : {numberList[guess]}, Length: {high}, Count : {iterationCount}");
+            Console.ReadLine();
         }
     }
 }
